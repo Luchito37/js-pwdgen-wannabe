62c356eeb6830bbe2cafac9c4db57a9c1101f767
Luchito37 <lucamatteotti01@gmail.com>
2022-05-17 18:25:18 +0200

    commit finale

--- type.js
+++ type.js
@@ -6,6 +6,8 @@ const annoNascitaUtente = prompt ("In che anno sei nato?") ;
 
 const luogoNascitaUtente = prompt ("Dove sei nato?")
 
+const colorePreferitoUtente = prompt ("Qual'è il tuo colore preferito?")
+
 const etaUtente = 2022 - annoNascitaUtente;
 
 const spanNomeUtente = document.getElementById("span-nome-utente");
@@ -14,6 +16,8 @@ spanNomeUtente.innerHTML = `<strong class="text-danger">
                             ${nomeUtente} ${cognomeUtente}
                             </strongs><br>
 
-                            <small class="text-white">You are <span class="text-danger">${etaUtente}</span> years old.</small>
+                            <small class="text-white">You are <span class="text-danger">${etaUtente}</span> years old,</small>
                             <br>
-                            <small class="text-white">And born in <span class="text-danger">${luogoNascitaUtente}</span></small>`;
+                            <small class="text-white">and born in <span class="text-danger">${luogoNascitaUtente}</span></small>
+                            <br>
+                            <small class="text-white">and your favourite color is <span class="text-danger">${colorePreferitoUtente}</span></small>.`;
